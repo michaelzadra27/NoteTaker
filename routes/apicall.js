b575bdad7db8bc91753ed8eb648c444ff98657a1
michaelzadra27 <michaelzadra27@gmail.com>
2021-06-13 15:54:29 -0700

    able to get data from page into object but haven't gotten it to save to the page yet

---
+++ routes/apicall.js
@@ -0,0 +1,54 @@
+const dataRout = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+// loads uuid module which allows for unique identifiers
+const { v4: uuidv4 } = require("uuid");
+//This route can read the db.json file where our data is saved and returns the data as JSON
+dataRout.get("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+        if (err) {
+            console.log("You have an Error");
+            return;
+        }
+        console.log(data);
+        var svData = []
+        return res.json(svData)
+    })
+});
+
+
+//creating rout to save notes
+dataRout.post("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+        if (err) {
+            console.log("Error");
+            return;
+        }
+        console.log(data)
+        var note = []
+        note = JSON.parse(data)
+        let notes = {
+            title: req.body.title,
+            text: req.body.text,
+            //uses the uuidv4 module to give the id a unique identifier
+            id: uuidv4(),
+        }
+
+        //pushing note to the db.json file 
+        note.push(notes)
+        fs.writeFile(
+            path.join(__dirname, "../db/db.json"),
+            JSON.stringify(note),
+            (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+              
+                return res.json(note);
+            }
+        )
+    })
+});
+
+module.exports = dataRout
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const path = require('path');
+//const pageRouter = require('express').Router();
+//importing html route file
+const htmlRouter = require("./routes/htmlpath")
+//importing api routing file
+const apiRouter = require("./routes/apicall")
+
+
+
 
 // Sets up the Express App
 
 const app = express();
 const PORT = 3000;
 
+
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+//allows the server to look at a static file without throwing an error
+app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-
+app.use(apiRouter)
+app.use(htmlRouter)
 
 
 
